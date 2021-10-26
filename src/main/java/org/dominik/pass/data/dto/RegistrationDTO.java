77e15cab30ab9fe05161e8a230549a816effe113
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-26 16:37:05 +0200

    Validation constraints added to RegistrationDTO class

--- src/main/java/org/dominik/pass/data/dto/RegistrationDTO.java
+++ src/main/java/org/dominik/pass/data/dto/RegistrationDTO.java
@@ -2,7 +2,11 @@ package org.dominik.pass.data.dto;
 
 import lombok.Getter;
 import lombok.ToString;
+import org.dominik.pass.utils.validators.EmailAddress;
+import org.dominik.pass.utils.validators.Hex;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -12,8 +16,21 @@ public final class RegistrationDTO implements Serializable {
 
   @Serial private static final long serialVersionUID = 1L;
 
+  @NotBlank(message = "{email.blank.message}")
+  @Length(max = 360, message = "{email.length.message}")
+  @EmailAddress(message = "{email.format.message}")
   private String email;
+
+  @NotBlank(message = "{password.blank.message}")
+  @Length(min = 64, max = 64, message = "{password.length.message}")
+  @Hex(message = "{password.hex.message}")
   private String password;
+
+  @NotBlank(message = "{salt.blank.message}")
+  @Length(min = 32, max = 32, message = "{salt.length.message}")
+  @Hex(message = "{salt.hex.message}")
   private String salt;
+
+  @Length(max = 255, message = "{reminder.length.message}")
   private String reminder;
 }
