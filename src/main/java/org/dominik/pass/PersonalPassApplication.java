8be4877b75b8b96d9b82c65e7167d1510d8d57b1
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-26 08:58:32 +0200

    Initial commit

---
+++ src/main/java/org/dominik/pass/PersonalPassApplication.java
@@ -0,0 +1,13 @@
+package org.dominik.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dominik/pass/PersonalPassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dominik.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
