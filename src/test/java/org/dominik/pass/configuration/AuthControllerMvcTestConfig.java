30d0f9d8ae9b50a41efcfe434a19bb9410eb1087
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-27 22:14:48 +0200

    Configuration for AuthController mvc tests added

---
+++ src/test/java/org/dominik/pass/configuration/AuthControllerMvcTestConfig.java
@@ -0,0 +1,12 @@
+package org.dominik.pass.configuration;
+
+import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@TestConfiguration
+@Import({SecurityTestConfig.class, JacksonTestConfig.class, PasswordEncoderTestConfig.class})
+@PropertySource(value = "classpath:mvc-test-configuration.yaml", factory = YamlPropertySourceFactory.class)
+public class AuthControllerMvcTestConfig {
+}
