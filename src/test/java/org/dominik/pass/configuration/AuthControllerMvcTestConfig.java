5d6d803e618d75d3cbc1d32e153df07977792b2c
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-28 21:05:03 +0200

    AuthControllerMvcTestConfig class updated to mock AccountService bean

--- src/test/java/org/dominik/pass/configuration/AuthControllerMvcTestConfig.java
+++ src/test/java/org/dominik/pass/configuration/AuthControllerMvcTestConfig.java
@@ -1,7 +1,9 @@
 package org.dominik.pass.configuration;
 
+import org.dominik.pass.services.definitions.AccountService;
 import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
@@ -9,4 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 @Import({SecurityTestConfig.class, JacksonTestConfig.class, PasswordEncoderTestConfig.class})
 @PropertySource(value = "classpath:mvc-test-configuration.yaml", factory = YamlPropertySourceFactory.class)
 public class AuthControllerMvcTestConfig {
+
+  @MockBean
+  AccountService accountService;
 }
