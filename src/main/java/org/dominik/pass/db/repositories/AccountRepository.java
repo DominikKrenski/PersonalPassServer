83ad819f85d4a1379e57cec6c2d7d3d71056d49b
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-29 10:51:16 +0200

    AccountRepository.existsByEmail() method added

--- src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
+++ src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
@@ -4,4 +4,5 @@ import org.dominik.pass.db.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+  boolean existsByEmail(String email);
 }
