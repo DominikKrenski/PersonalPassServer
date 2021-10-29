bc058a5712fe0e349715941e802b530cfe26f88f
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-29 10:51:35 +0200

    AccountServiceImpl.existsByEmail() method implemented

--- src/main/java/org/dominik/pass/services/definitions/AccountService.java
+++ src/main/java/org/dominik/pass/services/definitions/AccountService.java
@@ -5,4 +5,5 @@ import org.dominik.pass.data.dto.RegistrationDTO;
 
 public interface AccountService {
   AccountDTO register(RegistrationDTO dto);
+  boolean existsByEmail(String email);
 }
--- src/main/java/org/dominik/pass/services/implementations/AccountServiceImpl.java
+++ src/main/java/org/dominik/pass/services/implementations/AccountServiceImpl.java
@@ -1,9 +1,11 @@
 package org.dominik.pass.services.implementations;
 
+import lombok.NonNull;
 import org.dominik.pass.data.dto.AccountDTO;
 import org.dominik.pass.data.dto.RegistrationDTO;
 import org.dominik.pass.db.entities.Account;
 import org.dominik.pass.db.repositories.AccountRepository;
+import org.dominik.pass.errors.exceptions.ConflictException;
 import org.dominik.pass.services.definitions.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -22,6 +24,9 @@ public class AccountServiceImpl implements AccountService {
 
   @Override
   public AccountDTO register(RegistrationDTO dto) {
+    if (existsByEmail(dto.getEmail()))
+      throw new ConflictException("Email with given email already exists");
+
     Account.AccountBuilder builder = Account
         .builder()
         .email(dto.getEmail())
@@ -32,5 +37,10 @@ public class AccountServiceImpl implements AccountService {
       builder.reminder(dto.getReminder());
 
     return AccountDTO.fromAccount(accountRepository.save(builder.build()));
+  }
+
+  @Override
+  public boolean existsByEmail(@NonNull String email) {
+    return accountRepository.existsByEmail(email);
   }
 }
