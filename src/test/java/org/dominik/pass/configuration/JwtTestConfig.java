2a3c52a2c2616e205cfe5322f5b74103a6934c73
Dominik Kreński <dominik.krenski@gmail.com>
2021-10-31 11:36:39 +0100

    Tests for JwtConfigDev class added

---
+++ src/test/java/org/dominik/pass/configuration/JwtTestConfig.java
@@ -0,0 +1,27 @@
+package org.dominik.pass.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+@TestConfiguration
+@ConfigurationProperties
+@PropertySource(value = "classpath:jwt-dev.yaml", factory = YamlPropertySourceFactory.class)
+@Getter
+@Setter
+public class JwtTestConfig {
+  private String issuer;
+  private String key;
+  private Token accessToken = new Token();
+  private Token refreshToken = new Token();
+
+  @Getter
+  @Setter
+  public static class Token {
+    private String audience;
+    private int expiration;
+  }
+}
---
+++ src/test/java/org/dominik/pass/security/JwtPropsLoadTest.java
@@ -0,0 +1,32 @@
+package org.dominik.pass.security;
+
+import org.dominik.pass.configuration.JwtTestConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@Import(JwtTestConfig.class)
+@EnableConfigurationProperties(JwtTestConfig.class)
+@ActiveProfiles("dev")
+public class JwtPropsLoadTest {
+  @Autowired JwtTestConfig jwtConfig;
+
+  @Test
+  @DisplayName("should load properties")
+  void shouldLoadProperties() {
+    assertEquals("personal-pass.dev", jwtConfig.getIssuer());
+    assertEquals("gUkXn2r5u8x/A?D(G+KbPeShVmYq3s6v9y$B&E)H@McQfTjWnZr4u7w!z%C*F-Ja", jwtConfig.getKey());
+    assertEquals(120, jwtConfig.getAccessToken().getExpiration());
+    assertEquals("access", jwtConfig.getAccessToken().getAudience());
+    assertEquals(300, jwtConfig.getRefreshToken().getExpiration());
+    assertEquals("refresh", jwtConfig.getRefreshToken().getAudience());
+  }
+}
