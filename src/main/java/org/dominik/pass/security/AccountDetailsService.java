357117615a3fbe712d7db6897bd919e127e6299f
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-01 18:42:46 +0100

    AccountDetailsService component added

---
+++ src/main/java/org/dominik/pass/security/AccountDetailsService.java
@@ -0,0 +1,28 @@
+package org.dominik.pass.security;
+
+import org.dominik.pass.errors.exceptions.NotFoundException;
+import org.dominik.pass.services.definitions.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountDetailsService implements UserDetailsService {
+  private final AccountService accountService;
+
+  @Autowired
+  public AccountDetailsService(AccountService accountService) {
+    this.accountService = accountService;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    try {
+      return AccountDetails.fromDTO(accountService.findByEmail(email));
+    } catch (NotFoundException ex) {
+      throw new UsernameNotFoundException(ex.getMessage());
+    }
+  }
+}
