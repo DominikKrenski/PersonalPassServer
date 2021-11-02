fb386e13a7756ce4bffaa2bc2957da11861d9684
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-02 07:21:43 +0100

    JwtConfig fixed

--- src/main/java/org/dominik/pass/configuration/JwtConfig.java
+++ src/main/java/org/dominik/pass/configuration/JwtConfig.java
@@ -3,12 +3,15 @@ package org.dominik.pass.configuration;
 import lombok.Getter;
 import lombok.Setter;
 import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ConfigurationProperties
 @PropertySource(value = "classpath:jwt-${spring.profiles.active:dev}.yaml", factory = YamlPropertySourceFactory.class)
 @Getter
+@Setter
 public class JwtConfig {
   private String issuer;
   private String key;
