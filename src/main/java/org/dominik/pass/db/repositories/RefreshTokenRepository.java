2814e3b294354e4aa667ecbc19c213fd74f81c0d
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-03 07:31:12 +0100

    New methods added to RefreshTokenRepository

--- src/main/java/org/dominik/pass/db/repositories/RefreshTokenRepository.java
+++ src/main/java/org/dominik/pass/db/repositories/RefreshTokenRepository.java
@@ -2,6 +2,18 @@ package org.dominik.pass.db.repositories;
 
 import org.dominik.pass.db.entities.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+  @Modifying
+  @Query("DELETE FROM RefreshToken t WHERE t.account = (SELECT a FROM Account a WHERE a.publicId = :publicId)")
+  int deleteAllAccountTokens(@Param("publicId") UUID publicId);
+
+  List<RefreshToken> findByAccountPublicId(UUID publicId);
 }
