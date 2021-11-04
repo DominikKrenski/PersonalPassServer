08548c2a723af1fbbbebb283bac356a4809d4b4e
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-04 10:57:22 +0100

    AuthControllerMvcTestConfig updated to mock required dependencies

--- src/test/java/org/dominik/pass/configuration/AuthControllerMvcTestConfig.java
+++ src/test/java/org/dominik/pass/configuration/AuthControllerMvcTestConfig.java
@@ -1,17 +1,22 @@
 package org.dominik.pass.configuration;
 
+import org.dominik.pass.security.utils.JwtUtils;
 import org.dominik.pass.services.definitions.AccountService;
+import org.dominik.pass.services.definitions.RefreshTokenService;
 import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @TestConfiguration
 @Import({SecurityTestConfig.class, JacksonTestConfig.class, PasswordEncoderTestConfig.class})
 @PropertySource(value = "classpath:mvc-test-configuration.yaml", factory = YamlPropertySourceFactory.class)
 public class AuthControllerMvcTestConfig {
 
-  @MockBean
-  AccountService accountService;
+  @MockBean UserDetailsService detailsService;
+  @MockBean AccountService accountService;
+  @MockBean RefreshTokenService tokenService;
+  @MockBean JwtUtils jwtUtils;
 }
