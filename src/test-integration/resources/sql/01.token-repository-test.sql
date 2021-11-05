da70e0766c372c0281765596abc57b4be7eeacfa
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-05 15:42:12 +0100

    01.token-repository-test.sql refactored to use intengration schema

--- src/test-integration/resources/sql/01.token-repository-test.sql
+++ src/test-integration/resources/sql/01.token-repository-test.sql
@@ -1,19 +1,19 @@
-INSERT INTO test.accounts(email, password, salt, reminder)
+INSERT INTO integration.accounts(email, password, salt, reminder)
 VALUES
 ('dominik.krenski@gmail.com', 'b468879149f241f69ce185ee2cc1764047ece00f7aad0128053a12aee5be320c', '711882a4dc3dcb437eb6151c09025594', 'simple reminder'),
 ('dorciad@interia.pl', 'c468879149f241f69ce185ee2cc1764047ece00f7aad0528053a12aee5be320c', '741882a4dc3dcb437eb6151d09025f94', null);
 
-INSERT INTO test.refresh_tokens(token, account_id)
+INSERT INTO integration.refresh_tokens(token, account_id)
 VALUES
 (
   'refresh_token_1',
-  (SELECT id FROM test.accounts WHERE email = 'dominik.krenski@gmail.com')
+  (SELECT id FROM integration.accounts WHERE email = 'dominik.krenski@gmail.com')
 ),
 (
   'refresh_token_2',
-  (SELECT id FROM test.accounts WHERE email = 'dominik.krenski@gmail.com')
+  (SELECT id FROM integration.accounts WHERE email = 'dominik.krenski@gmail.com')
 ),
 (
   'refresh_token_3',
-  (SELECT id FROM test.accounts WHERE email = 'dominik.krenski@gmail.com')
+  (SELECT id FROM integration.accounts WHERE email = 'dominik.krenski@gmail.com')
 )
