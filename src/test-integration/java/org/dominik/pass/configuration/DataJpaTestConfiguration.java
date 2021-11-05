85e99affb1284bbeace4b290c39ecb6b16d1a9e7
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-05 15:43:22 +0100

    DataJpaTestConfiguration refactored to use standard configuration classes

--- src/test-integration/java/org/dominik/pass/configuration/DataJpaTestConfiguration.java
+++ src/test-integration/java/org/dominik/pass/configuration/DataJpaTestConfiguration.java
@@ -1,12 +1,9 @@
 package org.dominik.pass.configuration;
 
-import org.dominik.pass.utils.factories.YamlPropertySourceFactory;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @TestConfiguration
-@PropertySource(value = "classpath:data-jpa-test-configuration.yaml", factory = YamlPropertySourceFactory.class)
-@EnableJpaAuditing
+@Import(JpaConfigDev.class)
 public class DataJpaTestConfiguration {
 }
