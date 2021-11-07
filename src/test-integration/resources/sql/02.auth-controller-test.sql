92df9869e2a75ff5ed294d95fde9fbffca39dbfc
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-07 10:06:19 +0100

    Sql statements for AuthController Boot tests added to 02.auth-controller-test.sql

--- src/test-integration/resources/sql/02.auth-controller-test.sql
+++ src/test-integration/resources/sql/02.auth-controller-test.sql
@@ -1,5 +1,5 @@
 INSERT INTO integration.accounts(email, password, salt, reminder)
 VALUES
-('dominik.krenski@gmail.com', 'b468879149f241f69ce185ee2cc1764047ece00f7aad0128053a12aee5be320c', '711882a4dc3dcb437eb6151c09025594', 'simple message'),
+('dominik.krenski@gmail.com', '$2a$12$1rCLWvFfj1lcHm2lP1MJ/OyTNFseGh.mVdAGinD1gaOjjftBToa22', '711882a4dc3dcb437eb6151c09025594', 'simple message'),
 ('dorciad@interia.pl', 'c468879149f241f69ce185ee2cc1764047ece00f7aad0528053a12aee5be320c', '741882a4dc3dcb437eb6151d09025f94', null),
 ('krenska.dorota@gmail.com', '751882d4dc3dce437eb6751d09025f94', '741882a9dc3dcd437eb6151da9025f94', null);
