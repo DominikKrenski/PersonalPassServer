066d0ef8b8531db8fc4d673dd5733ec171cacdb5
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-15 15:05:14 +0100

    AccountRepository.findByPublicId() method added

--- src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
+++ src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
@@ -4,8 +4,10 @@ import org.dominik.pass.db.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
   boolean existsByEmail(String email);
   Optional<Account> findByEmail(String email);
+  Optional<Account> findByPublicId(UUID publicId);
 }
