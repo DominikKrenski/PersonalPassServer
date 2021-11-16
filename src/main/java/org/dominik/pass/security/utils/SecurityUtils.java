e0b8877b983999ee820f36fe978e18ddd1c3476d
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-16 11:14:50 +0100

    SecurityUtils component added

---
+++ src/main/java/org/dominik/pass/security/utils/SecurityUtils.java
@@ -0,0 +1,44 @@
+package org.dominik.pass.security.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dominik.pass.errors.api.ApiError;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Instant;
+
+@Component
+public class SecurityUtils {
+  private static final String SCHEME = "Bearer ";
+
+  private final ObjectMapper mapper;
+
+  @Autowired
+  public SecurityUtils(ObjectMapper mapper) {
+    this.mapper = mapper;
+  }
+
+  public String getToken(String content) {
+    // check if content starts with SCHEME, if not return null
+    if (!content.startsWith(SCHEME))
+      return null;
+
+    // return token
+    return content.substring(SCHEME.length());
+  }
+
+  public void prepareForbiddenResponse(HttpServletResponse response, String message) throws IOException {
+    ApiError apiError = ApiError
+        .builder()
+        .status(HttpStatus.FORBIDDEN)
+        .timestamp(Instant.now())
+        .message(message)
+        .build();
+
+    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+    response.getWriter().write(mapper.writeValueAsString(apiError));
+  }
+}
