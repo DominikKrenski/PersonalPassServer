8d041b343759bc9f3e76d86863ad91f07a5c1628
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-17 14:24:43 +0100

    Method for handling validation errors returned by @Validated annotation added to ApiControllerAdvice

--- src/main/java/org/dominik/pass/advices/ApiControllerAdvice.java
+++ src/main/java/org/dominik/pass/advices/ApiControllerAdvice.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.dominik.pass.errors.api.ApiError;
 import org.dominik.pass.errors.api.ValidationError;
 import org.dominik.pass.errors.exceptions.BaseException;
+import org.hibernate.validator.internal.engine.path.PathImpl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.time.Instant;
 import java.util.*;
 
@@ -96,6 +99,23 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     return new ResponseEntity<>(apiError, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
   }
 
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+    log.error("HANDLE CONSTRAINT VIOLATION EXCEPTION: " + Arrays.toString(ex.getStackTrace()));
+
+    List<ValidationError> validationErrors = prepareValidationErrors(ex.getConstraintViolations());
+
+    ApiError apiError = ApiError
+        .builder()
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .timestamp(Instant.now())
+        .message("Validation Error")
+        .errors(validationErrors)
+        .build();
+
+    return new ResponseEntity<>(apiError, HttpStatus.UNPROCESSABLE_ENTITY);
+  }
+
   @ExceptionHandler(BaseException.class)
   public ResponseEntity<Object> handleBaseException(BaseException ex) {
     log.error("HANDLE BASE EXCEPTION:" + Arrays.toString(ex.getStackTrace()));
@@ -121,6 +141,25 @@ public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
             messages.add(fieldError.getDefaultMessage());
             map.put(fieldError.getField(),
                 new ValidationError(fieldError.getField(), fieldError.getRejectedValue(), messages));
+          }
+        });
+
+    return new LinkedList<>(map.values());
+  }
+
+  private List<ValidationError> prepareValidationErrors(Set<ConstraintViolation<?>> violations) {
+    Map<String, ValidationError> map = new HashMap<>();
+
+    violations
+        .forEach(violation -> {
+          String fieldName = ((PathImpl) violation.getPropertyPath()).getLeafNode().getName();
+
+          if (map.containsKey(fieldName)) {
+            map.get(fieldName).getValidationMessages().add(violation.getMessage());
+          } else {
+            List<String> messages = new LinkedList<>();
+            messages.add(violation.getMessage());
+            map.put(fieldName, new ValidationError(fieldName, violation.getInvalidValue(), messages));
           }
         });
 
