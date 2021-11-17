b1d6b7af6b48058d8c843b3f0e0a034394c7ed70
Dominik Kreński <dominik.krenski@gmail.com>
2021-11-17 14:14:31 +0100

    Two custom queries added to AccountRepository and tested in AccountRepositoryIT

--- src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
+++ src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
@@ -2,6 +2,9 @@ package org.dominik.pass.db.repositories;
 
 import org.dominik.pass.db.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -10,4 +13,12 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
   boolean existsByEmail(String email);
   Optional<Account> findByEmail(String email);
   Optional<Account> findByPublicId(UUID publicId);
+
+  @Modifying
+  @Query("UPDATE Account a SET a.email = :newEmail WHERE a.email = :oldEmail")
+  int updateEmail(@Param("newEmail") String newEmail, @Param("oldEmail") String oldEmail);
+
+  @Modifying
+  @Query("UPDATE Account a SET a.reminder = :reminder WHERE a.email = :email")
+  int updateReminder(@Param("reminder") String reminder, @Param("email") String email);
 }
---
+++ src/test-integration/java/org/dominik/pass/db/repositories/AccountRepositoryIT.java
@@ -0,0 +1,55 @@
+package org.dominik.pass.db.repositories;
+
+import org.dominik.pass.configuration.DataJpaTestConfiguration;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@Import(DataJpaTestConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql("classpath:sql/02.auth-controller-test.sql")
+@ActiveProfiles("integration")
+class AccountRepositoryIT {
+
+  @Autowired AccountRepository accountRepository;
+
+  @Test
+  @DisplayName("should not update email if account does not exist")
+  void shouldNotUpdateEmailIfAccountNotExist() {
+    int result = accountRepository.updateEmail("dorciad@interia.pl", "dominik@yahoo.com");
+
+    assertEquals(0, result);
+  }
+
+  @Test
+  @DisplayName("should update email")
+  void shouldUpdateEmail() {
+    int result = accountRepository.updateEmail("dominik.krenski@yahoo.com", "dominik.krenski@gmail.com");
+
+    assertEquals(1, result);
+  }
+
+  @Test
+  @DisplayName("should not update reminder if account does not exist")
+  void shouldNotUpdateReminderIfAccountNonExist() {
+    int result = accountRepository.updateReminder("new dummy message", "dominik.krenski@yahoo.com");
+
+    assertEquals(0, result);
+  }
+
+  @Test
+  @DisplayName("should update reminder")
+  void shouldUpdateReminder() {
+    int result = accountRepository.updateReminder("new dummy reminder", "dominik.krenski@gmail.com");
+
+    assertEquals(1, result);
+  }
+}
