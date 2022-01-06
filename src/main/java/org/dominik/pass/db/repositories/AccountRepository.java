cb1e46318af702d9aa16009184c0f7d2a9be7a88
Dominik Kreński <dominik.krenski@gmail.com>
2022-01-06 15:51:04 +0100

    AccountRepository.updatePassword() query changed to update updated_at field

--- src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
+++ src/main/java/org/dominik/pass/db/repositories/AccountRepository.java
@@ -19,7 +19,7 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
   int updateEmail(@Param("newEmail") String newEmail, @Param("oldEmail") String oldEmail);
 
   @Modifying
-  @Query("UPDATE Account a SET a.password = :password, a.salt = :salt WHERE a.publicId = :publicId")
+  @Query("UPDATE Account a SET a.password = :password, a.salt = :salt, a.updatedAt = current_timestamp WHERE a.publicId = :publicId")
   int updatePassword(
     @Param("publicId") UUID publicId,
     @Param("password") String password,
